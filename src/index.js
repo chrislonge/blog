164c04d1c3ffc4c6d80f1dda14a1cbe04c093efb
Chris Longe <chrisalonge@gmail.com>
2017-12-03 16:51:50 -0500

    Router clean up. Removes app.js adds PostsIndex.js

--- src/index.js
+++ src/index.js
@@ -7,28 +7,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './style/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Home extends React.Component {
-    render() { return <div>Home!</div> }
-}
-class Hello extends React.Component {
-    render() { return <div>Hello!</div> }
-}
-class Goodbye extends React.Component {
-    render() { return <div>Goodbye!</div> }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={Home} />
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
